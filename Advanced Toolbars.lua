e4792b66579b8dd552e8dd0cff05116459b83369
Ferglerz <148029143+Ferglerz@users.noreply.github.com>
2025-05-18 02:38:23 -0400

    Add CFillion Profiler

--- Advanced Toolbars.lua
+++ Advanced Toolbars.lua
@@ -1,5 +1,7 @@
 -- Advanced Toolbars.lua
 
+_G.USE_PROFILER = false
+
 -- Get the script path
 local info = debug.getinfo(1, "S")
 _G.SCRIPT_PATH = info.source:match([[^@?(.*[\/])[^\/]-$]])
@@ -13,7 +15,6 @@ if not reaper.APIExists("ImGui_CreateContext") then
     reaper.ShowMessageBox("Please install ReaImGui extension.", "Error", 0)
     return
 end
-
 
 _G.UTILS = require("Utils.utils")
 _G.DRAWING = require("Utils.drawing")
@@ -248,6 +249,14 @@ function Loop()
         -- Set context to nil to allow garbage collection
         main_ctx = nil
     end
+end
+
+local profiler_path = reaper.GetResourcePath() .. '/Scripts/ReaTeam Scripts/Development/cfillion_Lua profiler.lua'
+if reaper.file_exists(profiler_path) and USE_PROFILER then
+  local profiler = dofile(profiler_path)
+  reaper.defer = profiler.defer
+  profiler.attachToWorld() -- after all functions have been defined
+  profiler.run()
 end
 
 reaper.defer(Loop)
